a46582b540df2590de45ecce814c38b1a0fbf61e
Habashy <habashy1989@gmail.com>
2022-03-18 14:12:24 +0200

    Commit befor form creation

--- trial/settings.py
+++ trial/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'sebha.apps.SebhaConfig',
     'products.apps.ProductsConfig',
     'students',
+    'watchman',
     # Capitalize first letter of app in the third word
 ]
 
@@ -122,9 +123,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR , 'static')
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR , 'project/static')
+                    
+]                   
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+ 
+ 
+MEDIA_ROOT = os.path.join(BASE_DIR , 'media')
+MEDIA_URL = '/media/'
--- trial/urls.py
+++ trial/urls.py
@@ -14,18 +14,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 import uu
+from xml.dom.minidom import Document
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('sebha/', include("sebha.urls")),
     path('products/', include("products.urls")),
     path('students/', include("students.urls")),
-
-
-
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # add include to imports above
 # path of tha app inside url of project
